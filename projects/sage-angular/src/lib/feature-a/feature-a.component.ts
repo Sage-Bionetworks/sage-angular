ffbb16fa017acbc84b5a7e28b0367eff6c3d986f
tschaffter <thomas.schaffter@gmail.com>
2021-04-16 08:53:51 -0700

    Fix component import issue (#3)

--- projects/sage-angular/src/lib/feature-a/feature-a.component.ts
+++ projects/sage-angular/src/lib/feature-a/feature-a.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'sage-feature-a',
-  template: '<p>feature-a works!</p>',
-  // templateUrl: './feature-a.component.html',
+  templateUrl: './feature-a.component.html',
   styleUrls: ['./feature-a.component.scss']
 })
 export class FeatureAComponent implements OnInit {
