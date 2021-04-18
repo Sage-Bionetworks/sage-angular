caf72fc219db2fa2697d7176dd15856c472ff2dc
tschaffter <thomas.schaffter@gmail.com>
2021-04-17 20:56:47 -0700

    Remove slider from demo app

--- projects/sage-angular-demo/src/app/app.module.ts
+++ projects/sage-angular-demo/src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatSliderModule } from '@angular/material/slider';
-
 import { GithubButtonModule } from '@sage-bionetworks/sage-angular/src/lib/github-button';
 import { HelloWorldModule } from '@sage-bionetworks/sage-angular/src/lib/hello-world';
 
@@ -17,7 +14,6 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSliderModule,
     AppRoutingModule,
     GithubButtonModule,
     HelloWorldModule
