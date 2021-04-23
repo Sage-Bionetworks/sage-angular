2fd33f86ae6736a670031733e311e7ac822ea0e3
tschaffter <thomas.schaffter@gmail.com>
2021-04-22 20:39:06 -0700

    Add initial navbar

--- projects/sage-angular/src/lib/navbar/navbar.component.ts
+++ projects/sage-angular/src/lib/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'sage-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  @Output()
+  login = new EventEmitter<Event>();
+
   constructor() { }
 
   ngOnInit(): void {}
--- projects/sage-angular/src/lib/navbar/navbar.module.ts
+++ projects/sage-angular/src/lib/navbar/navbar.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 
-
-
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [NavbarComponent]
 })
-export class NavbarModule { }
+export class NavbarModule {}
