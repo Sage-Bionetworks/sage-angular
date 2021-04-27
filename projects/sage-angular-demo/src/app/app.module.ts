6d936ac85658ced27c77abf090c0bb056d58a400
tschaffter <thomas.schaffter@gmail.com>
2021-04-27 16:01:29 -0700

    Remove prefix / from router links

--- projects/sage-angular-demo/src/app/app.module.ts
+++ projects/sage-angular-demo/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { FooterModule } from '@sage-bionetworks/sage-angular/src/lib/footer';
 import { GithubButtonModule } from '@sage-bionetworks/sage-angular/src/lib/github-button';
 import { NavbarModule } from '@sage-bionetworks/sage-angular/src/lib/navbar';
--- projects/sage-angular-demo/src/app/pages/homepage/homepage.module.ts
+++ projects/sage-angular-demo/src/app/pages/homepage/homepage.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { FooterModule } from '@sage-bionetworks/sage-angular/src/lib/footer';
+
 import { HomepageComponent } from './homepage.component';
 
 @NgModule({
   declarations: [HomepageComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FooterModule
+  ],
   exports: [HomepageComponent]
 })
 export class HomepageModule {}
