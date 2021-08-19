a7ad6896d99bfdc6c53a40a6ff558132d51b866d
tschaffter <thomas.schaffter@gmail.com>
2021-08-19 14:50:42 -0700

    Remove UMD warning

--- projects/sage-angular/src/lib/avatar/avatar.module.ts
+++ projects/sage-angular/src/lib/avatar/avatar.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { AvatarModule as NgxAvatarModule } from 'ngx-avatar';
+import { AvatarModule as NgxAvatarModule } from 'ngx-avatar';
 import { AvatarComponent } from './avatar.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, NgxAvatarModule],
   declarations: [AvatarComponent],
-  exports: [AvatarComponent]
+  exports: [AvatarComponent],
 })
 export class AvatarModule {}
--- projects/sage-angular/src/lib/navbar/navbar.module.ts
+++ projects/sage-angular/src/lib/navbar/navbar.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
-
 import { NavbarComponent } from './navbar.component';
 
 @NgModule({
+  imports: [CommonModule, RouterModule, MatButtonModule],
   declarations: [NavbarComponent],
-  imports: [
-    CommonModule,
-    RouterModule,
-    MatButtonModule
-  ],
-  exports: [NavbarComponent]
+  exports: [NavbarComponent],
 })
 export class NavbarModule {}
