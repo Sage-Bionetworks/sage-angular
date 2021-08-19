0fa9cc2cffa41fee8b27da877f7e5d373fa3fc6e
tschaffter <thomas.schaffter@gmail.com>
2021-08-18 21:06:53 -0700

    Update to Angular 12.2 but storybook still does not load global style

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,10 +6,7 @@ module.exports = {
     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-controls",
-    "@storybook/addon-actions",
     "storybook-addon-themes"
   ],
   core: {
