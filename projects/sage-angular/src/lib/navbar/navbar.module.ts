13d42053c35ae3d49e353d6cd6dce450888fc982
tschaffter <thomas.schaffter@gmail.com>
2021-08-20 08:55:16 -0700

    Import sage-avatar into navbar-user-button component

--- projects/sage-angular/src/lib/navbar/navbar.module.ts
+++ projects/sage-angular/src/lib/navbar/navbar.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { AvatarModule } from '@sage-bionetworks/sage-angular/src/lib/avatar';
 import { NavbarComponent } from './navbar.component';
 import { NavbarUserButtonComponent } from './navbar-user-button/navbar-user-button.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, MatButtonModule],
+  imports: [CommonModule, RouterModule, MatButtonModule, AvatarModule],
   declarations: [NavbarComponent, NavbarUserButtonComponent],
   exports: [NavbarComponent, NavbarUserButtonComponent],
 })
