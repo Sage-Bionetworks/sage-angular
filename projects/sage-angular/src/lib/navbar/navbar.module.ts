3fd328856fd0a9979940ec155b8bf8d5d98d68ec
tschaffter <thomas.schaffter@gmail.com>
2021-08-20 10:29:34 -0700

    The user button works but needs inputs

--- projects/sage-angular/src/lib/navbar/navbar.module.ts
+++ projects/sage-angular/src/lib/navbar/navbar.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { AvatarModule } from '@sage-bionetworks/sage-angular/src/lib/avatar';
 import { NavbarComponent } from './navbar.component';
 import { NavbarUserButtonComponent } from './navbar-user-button/navbar-user-button.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, MatButtonModule, AvatarModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatMenuModule,
+    AvatarModule,
+  ],
   declarations: [NavbarComponent, NavbarUserButtonComponent],
   exports: [NavbarComponent, NavbarUserButtonComponent],
 })
