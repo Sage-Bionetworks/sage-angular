9b29e624229c7274bf97b32ff102502331a1b4f5
tschaffter <thomas.schaffter@gmail.com>
2021-08-24 15:37:02 -0700

    Update format of more files

--- projects/sage-angular-demo/src/app/pages/explore/explore.module.ts
+++ projects/sage-angular-demo/src/app/pages/explore/explore.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-
 import { FooterModule } from '@sage-bionetworks/sage-angular/src/lib/footer';
 
 import { ExploreComponent } from './explore.component';
 
-const routes: Routes = [
-  { path: '', component: ExploreComponent }
-];
+const routes: Routes = [{ path: '', component: ExploreComponent }];
 
 @NgModule({
   declarations: [ExploreComponent],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes),
-    FooterModule
-  ],
-  exports: [ExploreComponent]
+  imports: [CommonModule, RouterModule.forChild(routes), FooterModule],
+  exports: [ExploreComponent],
 })
 export class ExploreModule {}
--- projects/sage-angular-demo/src/app/pages/signin/signin.component.ts
+++ projects/sage-angular-demo/src/app/pages/signin/signin.component.ts
@@ -3,7 +3,7 @@ import { Component, HostBinding, OnInit } from '@angular/core';
 @Component({
   selector: 'demo-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+  styleUrls: ['./signin.component.scss'],
 })
 export class SigninComponent {
   @HostBinding('class.main-content') readonly mainContentClass = true;
