92937bbc8f3911bad8a3f972432f3561072028eb
tschaffter <thomas.schaffter@gmail.com>
2021-08-24 15:16:08 -0700

    Add page title modul

---
+++ projects/sage-angular/src/lib/page-title/page-title.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageTitleService } from './page-title.service';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [],
+  providers: [PageTitleService],
+  exports: [],
+})
+export class PageTitleModule {}
--- projects/sage-angular/src/public-api.ts
+++ projects/sage-angular/src/public-api.ts
@@ -4,5 +4,6 @@
 export * from '@sage-bionetworks/sage-angular/src/lib/avatar';
 export * from '@sage-bionetworks/sage-angular/src/lib/footer';
 export * from '@sage-bionetworks/sage-angular/src/lib/navbar';
+export * from '@sage-bionetworks/sage-angular/src/lib/page-title';
 
 // notice that the internal utils are NOT exported from top level public-api.ts file
